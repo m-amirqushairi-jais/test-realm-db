0d5266692a85f951214ad88a48250fa0a8f13ea7
q-amir-j <q-amir@dxg.jp>
2023-03-24 07:48:09 +0800

    added and update tools csv to realm to supports reading all the csv in the folder

--- Tools/csv_to_realm_js/csv_to_realm.js
+++ Tools/csv_to_realm_js/csv_to_realm.js
@@ -2,65 +2,104 @@ const fs = require('fs');
 const path = require('path');
 const csvParser = require('csv-parser');
 const Realm = require('realm');
+const readline = require('readline');
 
-const inputCsv = '../../csv/Heroes.csv'; // Replace with your CSV file path
-const outputRealm = 'example.realm'; // Replace with your desired Realm database file path
-
-class CsvRow extends Realm.Object {}
-CsvRow.schema = {
-  name: 'CsvRow',
-  properties: {} // The schema will be updated dynamically based on the CSV file
-};
+const inputFolder = '../../csv'; // Replace with your CSV files folder path
+const outputRealm = 'cdc_test.realm'; // Replace with your desired Realm database file path
 
 async function main() {
-  const columns = await parseCsvHeader(inputCsv);
-  CsvRow.schema.properties = createRealmSchema(columns);
+  const csvFiles = fs.readdirSync(inputFolder).filter(file => file.endsWith('.csv'));
 
   const realmConfig = {
     path: outputRealm,
-    schema: [CsvRow],
+    schema: [],
     schemaVersion: 1,
   };
 
-  const realm = new Realm(realmConfig);
-  await processCsvFile(inputCsv, realm);
+  for (const csvFile of csvFiles) {
+    const filePath = path.join(inputFolder, csvFile);
+    const sheets = await readCsvSheets(filePath);
 
-  console.log(`CSV file '${inputCsv}' has been converted to Realm database '${outputRealm}'.`);
-  realm.close();
-}
+    for (const [sheetName, sheet] of Object.entries(sheets)) {
+      const schemaName = getSchemaName(csvFile, sheetName);
+      const columns = sheet.columns;
+      const schema = createRealmSchema(schemaName, columns);
 
-function parseCsvHeader(csvFile) {
-  return new Promise((resolve, reject) => {
-    const columns = [];
-    fs.createReadStream(csvFile)
-      .pipe(csvParser())
-      .on('headers', (headers) => {
-        resolve(headers);
-      })
-      .on('error', reject);
-  });
-}
+      realmConfig.schema.push(schema);
 
-function createRealmSchema(columns) {
-  const schema = {};
-  for (const column of columns) {
-    schema[column] = 'string';
+      const realm = new Realm(realmConfig);
+      await processCsvFile(realm, schema, sheet.rows);
+      realm.close();
+    }
   }
+
+  console.log(`CSV files in '${inputFolder}' have been converted to Realm database '${outputRealm}'.`);
+}
+
+async function readCsvSheets(filePath) {
+  const sheets = {};
+
+  const fileStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  let sheetName = 'Sheet1';
+  let sheetColumns = [];
+  let sheetRows = [];
+
+  for await (const line of rl) {
+    if (line === '---') {
+      sheets[sheetName] = { columns: sheetColumns, rows: sheetRows };
+      sheetName = `Sheet${Object.keys(sheets).length + 1}`;
+      sheetColumns = [];
+      sheetRows = [];
+    } else {
+      const row = line.split(',');
+
+      if (sheetColumns.length === 0) {
+        sheetColumns = row;
+      } else {
+        sheetRows.push(row);
+      }
+    }
+  }
+
+  sheets[sheetName] = { columns: sheetColumns, rows: sheetRows };
+
+  return sheets;
+}
+
+function getSchemaName(fileName, sheetName) {
+  const baseName = path.basename(fileName, '.csv');
+  return `${baseName}_${sheetName}`.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase()).replace(/\s+/g, '');
+}
+
+function createRealmSchema(schemaName, columns) {
+  const schema = {
+    name: schemaName,
+    properties: {}
+  };
+
+  for (const column of columns) {
+    schema.properties[column] = 'string';
+  }
+
   return schema;
 }
 
-function processCsvFile(csvFile, realm) {
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(csvFile)
-      .pipe(csvParser())
-      .on('data', (row) => {
-        realm.write(() => {
-          realm.create(CsvRow.schema.name, row);
-        });
-      })
-      .on('end', resolve)
-      .on('error', reject);
-  });
+async function processCsvFile(realm, schema, rows) {
+  for (const row of rows) {
+    const object = {};
+    for (let i = 0; i < schema.properties.length; i++) {
+      object[Object.keys(schema.properties)[i]] = row[i];
+    }
+
+    realm.write(() => {
+      realm.create(schema.name, object);
+    });
+  }
 }
 
 main().catch((error) => {
